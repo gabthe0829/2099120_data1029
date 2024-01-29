255cc086da621eaff3988c649c6ce28bc54d5ab8
Gabriel Theriault <120581491+gabthe0829@users.noreply.github.com>
2024-01-29 13:59:49 -0400

    Update gabriel_theriaultdevoir2.sql
    Exercise 5

--- gabriel_theriaultdevoir2.sql
+++ gabriel_theriaultdevoir2.sql
@@ -10,3 +10,5 @@ select title, price from titles where title like 'su%' or title like 'bu%';
 -- Exercise 4
 select title, price from titles where title not like 'su%' and title not like 'bu%';
 
+-- Exercise 5
+select title, price from titles where title not like 's%' and title not like 'b%' and title like '_o%';
