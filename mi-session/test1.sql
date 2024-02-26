bcb0d2cd584a887c87c66beb29086e0d779bcdb0
Gabriel Theriault <120581491+gabthe0829@users.noreply.github.com>
2024-02-26 14:16:45 -0400

    Update test1.sql
    Exercise 2 question #2

--- mi-session/test1.sql
+++ mi-session/test1.sql
@@ -1,2 +1,8 @@
 -- Exercise 2 #1
 select u.full_name as 'nom complet', r.name as 'Role'from user u join role r on u.role_id=r.id;
+
+-- Exercise 2 #2
+select p.name as 'designation', o.quantity as 'quantite' from product p join OrderLine o on p.id=o.product_id 
+union
+select p.name, o.quantity from product p, OrderLine o join invoice i on o.order_id=i.order_id where i.customer_id = 1;
+
