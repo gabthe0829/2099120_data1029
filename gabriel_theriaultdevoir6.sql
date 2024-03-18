ec30372aaa790519fc8d8c3ed19e29ee57c74375
Gabriel Theriault <120581491+gabthe0829@users.noreply.github.com>
2024-03-18 17:15:13 -0300

    Update gabriel_theriaultdevoir6.sql
    Exercise 2

--- gabriel_theriaultdevoir6.sql
+++ gabriel_theriaultdevoir6.sql
@@ -1,2 +1,5 @@
 -- Exercise 1
 select concat(e.fname, " " , e.lname), p.pub_name, max(job_lvl) from employees e join publishers p on e.pub_id=p.pub_id group by p.pub_id;
+
+-- Exercise 2
+select concat(fname, " " , lname), salary from employees where salary > (select salary from employees where concat(fname, " ", lname) = 'Norbert Zongo');
