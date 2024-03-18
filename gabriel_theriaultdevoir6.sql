671e2f4e249a9e385e34431fcb65d79b5b54e275
Gabriel Theriault <120581491+gabthe0829@users.noreply.github.com>
2024-03-18 17:39:21 -0300

    Update gabriel_theriaultdevoir6.sql
    Exercise 4

--- gabriel_theriaultdevoir6.sql
+++ gabriel_theriaultdevoir6.sql
@@ -6,3 +6,6 @@ select concat(fname, " " , lname), salary from employees where salary > (select 
 
 -- Exercise 3
 select concat(e.fname, " ", e.lname) , p.pub_name, p.country from employees e join publishers p on e.pub_id=p.pub_id where p.country = 'Canada';   
+
+-- Exercise 4
+select concat(fname, " ", lname), job_lvl from employees where not job_lvl = 'SEINIOR' or 'MANAGER';
