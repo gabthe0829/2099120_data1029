274cb07d6e7b9264c500fc95931423c95287e258
Gabriel Theriault <120581491+gabthe0829@users.noreply.github.com>
2024-03-18 17:58:18 -0300

    Update gabriel_theriaultdevoir6.sql
    Exercise 5

--- gabriel_theriaultdevoir6.sql
+++ gabriel_theriaultdevoir6.sql
@@ -9,3 +9,6 @@ select concat(e.fname, " ", e.lname) , p.pub_name, p.country from employees e jo
 
 -- Exercise 4
 select concat(fname, " ", lname), job_lvl from employees where not job_lvl = 'SEINIOR' or 'MANAGER';
+
+-- Exercise 5
+select concat(fname, " ", lname), salary from employees where salary > (select avg(e.salary) from employees e join publishers p on e.pub_id=p.pub_id); 
