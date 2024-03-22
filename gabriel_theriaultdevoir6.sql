728a76818c724511a2ac9227c3be120c5d5ae096
Gabriel Theriault <120581491+gabthe0829@users.noreply.github.com>
2024-03-22 13:20:32 -0300

    Update gabriel_theriaultdevoir6.sql
    Exercise 7

--- gabriel_theriaultdevoir6.sql
+++ gabriel_theriaultdevoir6.sql
@@ -23,3 +23,7 @@ union
 select concat(fname, " ", lname), min(salary), job_lvl from employees where job_lvl = 'SEINIOR'
 union
 select concat(fname, " ", lname), min(salary), job_lvl from employees where job_lvl = 'MANAGER'; 
+
+-- Exercise 7
+select t.title, t.type, sum(s.qty) from titles t join sales s on t.title_id = s.title_id
+group by t.title, t.type order by sum(s.qty) desc;
